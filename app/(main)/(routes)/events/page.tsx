8284187ee07646e91d7054f423071e10f36d327d
KushagraPS <165717541+KushagraPS@users.noreply.github.com>
2024-04-23 11:32:16 +0530

    Update page.tsx

--- app/(main)/(routes)/events/page.tsx
+++ app/(main)/(routes)/events/page.tsx
@@ -131,10 +131,10 @@ export function CardHoverEffectDemo() {
 }
 export const projects = [
   {
-    title: "Stripe",
+    title: "AWS (Amazon Web Services)",
     description:
-      "A technology company that builds economic infrastructure for the internet.",
-    link: "https://stripe.com",
+      "Confused by Cloud options? Don't worry, we've got you covered. Our AWS Evaluation Calculator simplifies choosing the right Amazon Web Services plan for your startup. No tech jargon, just easy cloud solutions!",
+    link: "https://aws.amazon.com/free/?gclid=Cj0KCQjwlZixBhCoARIsAIC745DcJXm20SnMcnkEW-QbaaHd3NNAcFbintfsckxjmI9aJIaC99YDWrQaAlH6EALw_wcB&trk=14a4002d-4936-4343-8211-b5a150ca592b&sc_channel=ps&ef_id=Cj0KCQjwlZixBhCoARIsAIC745DcJXm20SnMcnkEW-QbaaHd3NNAcFbintfsckxjmI9aJIaC99YDWrQaAlH6EALw_wcB:G:s&s_kwcid=AL!4422!3!453325184782!e!!g!!aws!10712784856!111477279771&all-free-tier.sort-by=item.additionalFields.SortRank&all-free-tier.sort-order=asc&awsf.Free%20Tier%20Types=*all&awsf.Free%20Tier%20Categories=*all",
   },
   {
     title: "Netflix",
