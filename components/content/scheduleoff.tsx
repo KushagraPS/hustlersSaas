5843393a811c1c01c21b837d9621d338199ccb9e
KushagraPS <165717541+KushagraPS@users.noreply.github.com>
2024-04-24 16:27:23 +0530

    Update scheduleoff.tsx

--- components/content/scheduleoff.tsx
+++ components/content/scheduleoff.tsx
@@ -20,73 +20,59 @@ export default function Schedule() {
 
     const preWorkshopData = [
         {
-            day: 'Free STARTUP Day 1',
-            time: '27 Feb, 2024',
-            name: 'Workshop on cloud computing ',
-            date: '27th  February 2024  10:00 AM onwards',
-            duration: '5 hours',
-            expert: [
+            day: 'Founders Meetup - Bangalore Edition',
+            time: '6:30 - 8:30pm IST',
+            name: 'Founders Meetup - Bangalore Edition',
+            date: 'Sunday, 27th April',
+            duration: '2 hours',
+            events: [
                 {
-                    name: '1. Ms. Samistha Goswami',
-                    description: 'Experienced Master Trainer with a demonstrated history of working in the higher education industry, Skilled in AI & ML .He trained 2000+ students.'
+                    name: 'Founders War Room',
+                    description: ''
                 },
-                {
-                    name: '2. Mr.Palwinder Singh',
-                    description: 'He has  8+ years experience as a Web application developer,  Worked as Subject Matter expert in IBM Career education .Having good experience on Python , Machine learning, Deep learning, Data Analytics, DevOps, MySQL, MongoDB.'
-                },
+               
             ],
             link: 'CC_Workshop_CE[1].pdf',
         },
         {
-            day: 'Pre WORKSHOP Day 2',
-            time: '28 Feb, 2024',
-            name: 'Workshop on Cyber security  ',
-            date: '28th February 2024  10:00 AM onwards',
-            duration: '3 hours',
+            day: 'Coming Soon',
+            time: '-',
+            name: '- ',
+            date: '-',
+            duration: '-',
             expert: [
                 {
-                    name: '1. Mr.Partho Pandya (Cyber Security Analyst)',
-                    description: 'He is a dedicated Cyber Security Researcher and VAPT (Vulnerability Assessment and Penetration Testing) Analyst. He brings a wealth of expertise to the dynamic field of cybersecurity. His academic background, coupled with hands- on experience has allowed him to excel in various domains, earning recognition as a top performer in platforms like TryHackMe, where he consistently ranks in the top 4%. Moreover, he has solved 30+ cases with cyber cell and conducted 50+ conferences.'
+                    name: '-',
+                    description: '-'
                 },
                 {
-                    name: '2. Mr. Akshar Ratnani (Cyber Security Analyst)',
-                    description: 'He has expertise in the vast field of Cybersecurity domains such as VAPT, SOC, Compliance and Digital Forensics. He has Skills  Network Security ,Penetration Testing,Security Awareness Training, Malware Analysis, Bug Bounty Hunting, Forensics Analysis'
+                    name: '-',
+                    description: '-'
                 },
             ],
             link: 'CS_Workshop_CE[1].pdf',
         },
         {
-            day: 'Pre WORKSHOP Day 3',
-            time: '29 Feb, 2024',
-            name: 'Workshop on Full stack with python ',
-            date: '29th February 2024  10:00 AM onwards',
-            duration: '4 hours',
+            day: 'Coming Soon',
+            time: '-',
+            name: '- ',
+            date: '-',
+            duration: '',
             expert: [
                 {
-                    name: '1. Mr. Jigar Thakkar (Developer & Trainer)',
-                    description: 'Trained more than 5000+ students with a mixed category for training Students, experienced IT individuals and students looking for placements in Java. Subjects covered in all this training was Office, Data Structure, C, C++, Data Structure, HTML, CSS, Java Script, Python, IOT, Core Java, Advance Java and Oracle'
+                    name: '-',
+                    description: '-'
                 },
                 {
-                    name: '2. Ms. Anjali Patel (Developer & Trainer)',
-                    description: 'Passionate Android – Python Trainer with 10 years of experience in Android / Python as Trainer. Good Knowledge in Mobile development (Android, Flutter, React Native) and Web Development (Python, Django Web framework). Strong experience in Training environments. Proficient in object-oriented design, data structures, Problem solving and debugging. Fully conversant with the latest IT Innovations and ability to produce high quality deliverable within estimated Time constraints in pressurized environments.'
+                    name: '-',
+                    description: '-'
                 },
             ],
             link: 'Python_Workshop_CE[1].pdf',
         },
     ];
-    const scheduleData = [
 
-        {
-            day: 'Day 1 - Events',
-            time: '1 March, 2024',
-            link : 'rules/Day1.pdf',
-        },
-        {
-            day: 'Day 2 - Events',
-            time: '2 March, 2024',
-            link : 'rules/Day2.pdf',
-        },
-    ];
+   
     // { day: 'Day 1 - Workshops', time: '10:00 AM - 5:00 PM' },
 
     return (
@@ -123,17 +109,17 @@ export default function Schedule() {
                                             <DrawerDescription>Date: {event.date}</DrawerDescription>
                                             <DrawerDescription>Duration: {event.duration}</DrawerDescription>
                                             <Separator className="my-2" />
-                                            {Array.isArray(event.expert) && event.expert.map((expert, index) => (
+                                            {Array.isArray(event.events) && event.events.map((events, index) => (
                                                 <React.Fragment key={index}>
-                                                    <DrawerTitle className="text-left mt-2 bg-gradient-to-r from-amber-500 to-pink-500 bg-clip-text text-transparent">Expert: {expert.name}</DrawerTitle>
-                                                    <DrawerDescription className="text-left text-xs">{expert.description}</DrawerDescription>
+                                                    <DrawerTitle className="text-left mt-2 bg-gradient-to-r from-amber-500 to-pink-500 bg-clip-text text-transparent">Event: {events.name}</DrawerTitle>
+                                                    <DrawerDescription className="text-left text-xs">{events.description}</DrawerDescription>
                                                 </React.Fragment>
                                             ))}
                                             <Separator className="my-2" />
                                             <Link href={event.link} target="_blank">
                             <div className="pt-3 flex items-center">
-                              <FaBook className="text-amber-500 " />
-                              <DrawerDescription className="px-2 font-bold uppercase bg-gradient-to-r from-amber-500 to-pink-500 bg-clip-text text-transparent">More Details</DrawerDescription>
+                            
+                            
                             </div>
                           </Link>
                                         </DrawerHeader>
@@ -143,7 +129,7 @@ export default function Schedule() {
                                                 href={"https://forms.gle/DCi3RZKQgGLqAPdi6"}
                                                 target="_blank"
                                                 className="mx-auto">
-                                                <Button variant="primary" size={"sm"}>Register for Workshop</Button>
+                                                <Button variant="primary" size={"sm"}>Register</Button>
                                             </Link>
                                             <DrawerClose asChild>
                                                 <Button variant="outline">Cancel</Button>
@@ -154,17 +140,7 @@ export default function Schedule() {
                             </Drawer>
                         </div>
                     ))}
-                    {scheduleData.map((item, index) => (
-                        <Link href={item.link} target="_blank" key={index}>
-                            <div 
-                                className="grid grid-cols-1 md:grid-cols-2 gap-1 items-center border border-indigo-400 rounded-xl px-4 py-4 mx-auto" 
-                                style={{ cursor: 'pointer' }}
-                            >
-                                <h3 className="text-lg font-bold bg-gradient-to-r from-indigo-400 to-cyan-400 bg-clip-text text-transparent">{item.day}</h3>
-                                <p className="text-sm text-gray-500 dark:text-gray-400">{item.time}</p>
-                            </div>
-                        </Link>
-                    ))}
+                  
                 </div>
                 {/* <div className="flex justify-center mx-auto">
                     <Link href="events">
