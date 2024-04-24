abf5856d1531042ffaff07b6e405cd25ec60c76f
KushagraPS <165717541+KushagraPS@users.noreply.github.com>
2024-04-24 16:09:17 +0530

    Update scheduleoff.tsx

--- components/content/scheduleoff.tsx
+++ components/content/scheduleoff.tsx
@@ -95,9 +95,9 @@ export default function Schedule() {
                 <div className="flex flex-col items-center justify-center space-y-4 text-center">
                     <div className="space-y-2">
                        
-                        <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Event Schedule</h2>
+                        <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Your Week at a Glance</h2>
                         <p className="max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-                            Check out the schedule for the week-long event. Plan your participation accordingly.
+                        Do not miss a beat! Explore the week-long event schedule to craft your ideal experience.
                         </p>
                     </div>
                 </div>
