a91d674ded04c8bf709e278471a41e16f08f7871
KushagraPS <165717541+KushagraPS@users.noreply.github.com>
2024-05-02 14:41:52 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'images.unsplash.com',
-          port: '',
-          pathname: '/**',
-        },
-      ],
+    domains: ['pedalstart.com', 'images.unsplash.com', 'onlinemediacafe.com','encrypted-tbn0.gstatic.com','media.licdn.com','images.yourstory.com','img-cdn.thepublive.com','s3.ap-south-1.amazonaws.com','viestories.com'], // Add 'pedalstart.com' to the list of domains
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
   // Add metadata configuration here
   
